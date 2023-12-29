bce34b72c171bb2e086812c70c534d61b3c9d57e
jin <me@caijin.tech>
2023-12-30 00:29:21 +0800

    fix: rss gen script

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -1,13 +1,30 @@
 const {promises: fs} = require('fs')
 const path = require('path')
-const RSS = require('rss')
+// https://github.com/jpmonette/feed
+const Feed = require('feed').Feed
 const matter = require('gray-matter')
 
 async function generate() {
-    const feed = new RSS({
+    const feed = new Feed({
         title: "Jin's Blog",
-        site_url: 'https://yoursite.com',
-        feed_url: 'https://yoursite.com/feed.xml',
+        description: "This is my personal feed!",
+        id: "http://example.com/",
+        link: "http://example.com/",
+        language: "en", // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+        image: "http://example.com/image.png",
+        favicon: "http://example.com/favicon.ico",
+        copyright: "All rights reserved 2024, Jin",
+        // updated: new Date(2013, 6, 14), // optional, default = today
+        // generator: "awesome", // optional, default = 'Feed for Node.js'
+        // feedLinks: {
+        //     json: "https://example.com/json",
+        //     atom: "https://example.com/atom"
+        // },
+        // author: {
+        //     name: "John Doe",
+        //     email: "johndoe@example.com",
+        //     link: "https://example.com/johndoe"
+        // }
     })
 
     const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
@@ -23,19 +40,26 @@ async function generate() {
                 const dirResult = await flattenPost(dirNameList, currentDirPath)
                 result.push(...dirResult)
             } else {
-                result.push(path.join(dirPath, nameList[i]))
+                result.push({
+                    absolutePath: path.join(dirPath, nameList[i]),
+                    relativePath: path.join(dirPath, nameList[i]).replace(path.join(__dirname, '../pages/posts'), ''),
+                    name: nameList[i]
+                })
             }
         }
 
-        return result.filter(v => {
+        return result.filter(item => {
+            const v = item.absolutePath;
             return (v.indexOf('.mdx') !== -1 || v.indexOf('.md') !== -1) && v.indexOf('index.') === -1
         });
     }
 
     const allPosts = await flattenPost(posts, path.join(__dirname, '..', 'pages', 'posts'));
+    console.log('allPosts', allPosts)
 
     await Promise.all(
-        allPosts.map(async (filePath) => {
+        allPosts.map(async (item) => {
+            const filePath = item.absolutePath;
 
             const content = await fs.readFile(filePath)
 
@@ -43,7 +67,7 @@ async function generate() {
 
             feed.item({
                 title: frontmatter.data.title,
-                url: '/posts/' + filePath.replace(/\.mdx?/, ''),
+                url: '/post' + item.relativePath,
                 date: frontmatter.data.date,
                 description: frontmatter.data.description,
                 categories: frontmatter.data.tag.split(', '),
