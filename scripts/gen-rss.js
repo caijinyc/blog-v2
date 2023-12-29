10f2f833c8b33028ad43c0c17398f780bc9db00f
jin <me@caijin.tech>
2023-12-30 00:13:46 +0800

    fix: rss gen script

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -1,38 +1,58 @@
-const { promises: fs } = require('fs')
+const {promises: fs} = require('fs')
 const path = require('path')
 const RSS = require('rss')
 const matter = require('gray-matter')
 
 async function generate() {
-  const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
-  })
-
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
-
-  await Promise.all(
-    posts.map(async (name) => {
-      if (name.startsWith('index.')) return
-
-      const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
-      )
-      const frontmatter = matter(content)
-
-      feed.item({
-        title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
-        date: frontmatter.data.date,
-        description: frontmatter.data.description,
-        categories: frontmatter.data.tag.split(', '),
-        author: frontmatter.data.author,
-      })
+    const feed = new RSS({
+        title: "Jin's Blog",
+        site_url: 'https://yoursite.com',
+        feed_url: 'https://yoursite.com/feed.xml',
     })
-  )
 
-  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+    const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
+
+    const flattenPost = async (nameList, dirPath) => {
+        const result = [];
+        for (let i = 0; i < nameList.length; i++) {
+            const isDir = nameList[i].indexOf('.') === -1
+            if (isDir) {
+                const dirName = nameList[i]
+                const currentDirPath = path.join(dirPath, dirName)
+                const dirNameList = await fs.readdir(currentDirPath)
+                const dirResult = await flattenPost(dirNameList, currentDirPath)
+                result.push(...dirResult)
+            } else {
+                result.push(path.join(dirPath, nameList[i]))
+            }
+        }
+
+        return result.filter(v => {
+            return (v.indexOf('.mdx') !== -1 || v.indexOf('.md') !== -1) && v.indexOf('index.') === -1
+        });
+    }
+
+    const allPosts = await flattenPost(posts, path.join(__dirname, '..', 'pages', 'posts'));
+
+    await Promise.all(
+        allPosts.map(async (filePath) => {
+
+            const content = await fs.readFile(filePath)
+
+            const frontmatter = matter(content)
+
+            feed.item({
+                title: frontmatter.data.title,
+                url: '/posts/' + filePath.replace(/\.mdx?/, ''),
+                date: frontmatter.data.date,
+                description: frontmatter.data.description,
+                categories: frontmatter.data.tag.split(', '),
+                author: frontmatter.data.author,
+            })
+        })
+    )
+
+    await fs.writeFile('./public/feed.xml', feed.xml({indent: true}))
 }
 
 generate()
